1da2e482d2791712eaedd7d58523b249f8d02466
Abdulkadir1614 <abdukadir1614@gmail.com>
2024-09-05 15:05:57 +0800

    Update challenge.js
    Done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,8 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+let bill = 275; 
+let tip = (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;
+let total = bill + tip;
+
+console.log(`The bill was ${bill}, the tip was ${tip.toFixed(2)}, and the total value ${total.toFixed(2)}`);
